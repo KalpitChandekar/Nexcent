8b27c8f2dd4950ea5601e7cfb0f331b287e116ca
Kalpit <kalpitchandekar1736@gmail.com>
2024-11-01 13:37:22 +0530

    [feat]: Add Hero Section

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,13 @@
+import Hero from "@/components/Hero";
 import Navbar from "@/components/Navbar";
 
 const page = () => {
   return (
     <div>
       <Navbar />
+      <div className="bg-hero h-[33rem]">
+        <Hero />
+      </div>
     </div>
   );
 };
